dd1001770ed08f852ffeda30502de223fbe2c231
prakashkaruppusamy <prakashbtech87@gmail.com>
2024-06-30 18:50:05 +0530

    First version

---
+++ src/main/java/com/prakash/aop/employeeaop/EmployeeaopApplication.java
@@ -0,0 +1,13 @@
+package com.prakash.aop.employeeaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeaopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeaopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prakash/aop/employeeaop/EmployeeaopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prakash.aop.employeeaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeaopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
